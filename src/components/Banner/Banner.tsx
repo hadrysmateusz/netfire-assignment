f166f817164c4b7f831e4cf0527d54428207b294
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2024-11-06 14:21:28 +0100

    fix banner spacing issue

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -8,7 +8,7 @@ export const Banner = ({ children }: Props) => {
   return (
     <section>
       <div className="w-full h-[62px] flex justify-center items-center text-center uppercase font-secondary text-sm bg-gray-2 text-gray-6 font-medium tracking-[0.07em]">
-        {children}
+        <div>{children}</div>
       </div>
       <GradientSeparator />
     </section>
